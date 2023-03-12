15faa853f747cd26221670d289cd7a7006f64249
Bloc4488 <bloc4488@gmail.com>
2023-03-13 00:05:02 +0100

    Adding all files to repo. Adding DB to project

---
+++ Market_viewer/Wallet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market_viewer
+{
+    internal class Wallet
+    {
+        public int id {  get; set; }
+
+        private float value;
+
+        private int ticker_id;
+
+
+        public Wallet() { }
+
+        public Wallet(int id, float value, int ticker_id)
+        {
+            this.id = id;
+            this.value = value;
+            this.ticker_id = ticker_id;
+        }
+
+    }
+}
