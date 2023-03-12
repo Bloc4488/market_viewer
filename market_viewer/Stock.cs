4dc995a122f381fa77e7af4c9270d990e95637b9
piechoor <jakub.zajac40@gmail.com>
2023-03-12 22:26:46 +0100

    Add project files.

---
+++ market_viewer/Stock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace market_viewer
+{
+    class Stock
+    {
+        private string ticker;
+        private bool ifFavorite;
+        public string Ticker   // property
+        {
+            get { return ticker; }
+        }
+
+
+        Stock(string ticker)
+        {
+            this.ticker = ticker;
+            this.ifFavorite = false;
+        }
+
+        public void SetFavorite() { ifFavorite = true; }
+
+    }
+}
---
+++ market_viewer/Wallet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace market_viewer
+{
+    class Wallet
+    {
+        private List<Stock> stocksList;
+        private float walletValue;
+
+        Wallet()
+        {
+            stocksList = new List<Stock>();
+            walletValue = 0;
+        }
+    }
+}
